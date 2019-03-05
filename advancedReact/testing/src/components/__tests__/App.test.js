3a124e08ce0c671a33b77a6d0399f3bb71938838
Gagan Shantha Kumar <gaganskumar9@gmail.com>
2019-03-05 00:25:25 -0500

    Enzyme setup

--- advancedReact/testing/src/components/__tests__/App.test.js
+++ advancedReact/testing/src/components/__tests__/App.test.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from '../App';
+import CommentBox from '../CommentBox';
+import CommentList from '../CommentList';
 
 it('shows a comment box', () => {
+  /* *** Not required from Enzyme ***
   const div = document.createElement('div');
-
   ReactDOM.render(<App />, div);
-
   expect(div.innerHTML).toContain('Comment Box');
-
   ReactDOM.unmountComponentAtNode(div);
+  */
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentBox).length).toEqual(1);
+
+});
+
+it('shows a comment list', () => {
+  const wrapped = shallow(<App />);
+
+  expect(wrapped.find(CommentList).length).toEqual(1);
+
 });
