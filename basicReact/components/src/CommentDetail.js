6098589070ea060820e0c809360a494e007e91c7
Gagan Shantha Kumar <gaganskumar9@gmail.com>
2019-03-09 21:19:32 -0500

    Added Props

--- basicReact/components/src/CommentDetail.js
+++ basicReact/components/src/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = () => {
+const CommentDetail = (props) => {
+
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img alt="avatar" src={faker.image.avatar()}/>
+        <img alt="avatar" src={props.avatar}/>
       </a>
       <div className="content">
         <a href="/" className="author">
-          Sam
+          {props.author}
         </a>
         <div className="metadata">
-          <span className="date">Today at 6:00 PM</span>
+          <span className="date">{props.timeAgo}</span>
         </div>
-        <div className="text">Nice blog post!</div>
+        <div className="text">{props.message}</div>
       </div>
     </div>
   );
--- basicReact/components/src/index.js
+++ basicReact/components/src/index.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import faker from 'faker';
 
 const App = () => {
   return (
     <div className=" ui container comments">
-      <CommentDetail />
-      <CommentDetail />
-      <CommentDetail />
+      <CommentDetail author="Gagan"
+                    timeAgo=" Today at 4:45 PM"
+                    avatar={faker.image.avatar()}
+                    message="I am the son"
+      />
+      <CommentDetail author="Shantha"
+                     timeAgo="Today at 2:00 PM"
+                     avatar={faker.image.avatar()}
+                     message="I am the wife"
+      />
+      <CommentDetail author="Kumar"
+                     timeAgo="Yesterdat at 5:00 PM"
+                     avatar={faker.image.avatar()}
+                     message="I am the husband" />
     </div>
   );
 };
