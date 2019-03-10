1d75ea9e3c0f3dd5260caf499aab40d879d7c0f2
Gagan Shantha Kumar <gaganskumar9@gmail.com>
2019-03-09 21:42:46 -0500

    Props completed

--- basicReact/components/src/index.js
+++ basicReact/components/src/index.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 import faker from 'faker';
 
 const App = () => {
   return (
     <div className=" ui container comments">
-      <CommentDetail author="Gagan"
-                    timeAgo=" Today at 4:45 PM"
-                    avatar={faker.image.avatar()}
-                    message="I am the son"
-      />
-      <CommentDetail author="Shantha"
-                     timeAgo="Today at 2:00 PM"
-                     avatar={faker.image.avatar()}
-                     message="I am the wife"
-      />
-      <CommentDetail author="Kumar"
-                     timeAgo="Yesterdat at 5:00 PM"
-                     avatar={faker.image.avatar()}
-                     message="I am the husband" />
+      <ApprovalCard>
+        <h4> Warning! </h4>
+        <div> Are you sure? </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Gagan"
+                      timeAgo=" Today at 4:45 PM"
+                      avatar={faker.image.avatar()}
+                      message="I am the son"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Shantha"
+                       timeAgo="Today at 2:00 PM"
+                       avatar={faker.image.avatar()}
+                       message="I am the wife"
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail author="Kumar"
+                       timeAgo="Yesterdat at 5:00 PM"
+                       avatar={faker.image.avatar()}
+                       message="I am the husband"
+        />
+      </ApprovalCard>
+
     </div>
   );
 };
