d0035243f4c22e0145384aa349a95f299fa825ec
Gagan Shantha Kumar <gaganskumar9@gmail.com>
2019-03-11 18:14:29 -0400

    API call and Async await

--- basicReact/pics/src/components/App.js
+++ basicReact/pics/src/components/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: {
+        query: term
+      },
+      headers: {
+        Authorization: 'Client-ID 857f89c5e08f3bffc191f5cadf85d2ad38f22ba0e425ec339aa90d3d15e0dc12'
+      }
+    });
+    console.log(response.data.results);
   }
 
   render() {
